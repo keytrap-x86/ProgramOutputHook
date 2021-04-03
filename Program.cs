fcb8de0cf161950fcb3b29f54d41ad5a3fd1b107
Keytrap <badniiw@gmail.com>
2021-04-03 12:18:09 +0200

    Translation en English + add readme

--- Program.cs
+++ Program.cs
@@ -5,33 +5,33 @@ using System.Text;
 
 namespace ProgramOutputHook
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.Write("Veuillez entrer le nom du programme à écouter : ");
+            Console.Write("Enter the program's name that you wish to listen to : ");
             var programToListenTo = Console.ReadLine();
 
             while(!File.Exists(programToListenTo))
             {
-                Console.WriteLine($"Impossible de trouver le fichier '{programToListenTo}' dans le répertoire actuel.");
-                Console.Write("Veuillez entrer le nom du programme à écouter : ");
+                Console.WriteLine($"Cannot find '{programToListenTo}' in the current directory.");
+                Console.Write("Enter the program's name that you wish to listen to : ");
                 programToListenTo = Console.ReadLine();
             }
 
-            Console.Write("Veuillez entrer un argument à donner au programme (facultatif) : ");
+            Console.Write("Enter some arguments (press ENTER for none) : ");
             var programArgument = Console.ReadLine();
 
             StartProcess(programToListenTo, programArgument);
 
-            Console.WriteLine("Terminé. [ENTREE] Recommencer [Autre] Quitter...");
+            Console.WriteLine("Done. [ENTER] Restart [Other] Exit...");
             var key = Console.ReadKey();
 
             while(key.Key == ConsoleKey.Enter)
             {
                 StartProcess(programToListenTo, programArgument);
 
-                Console.WriteLine("Terminé. [ENTREE] Recommencer [Autre] Quitter...");
+                Console.WriteLine("Done. [ENTER] Restart [Other] Exit...");
                 key = Console.ReadKey();
             }
         }
@@ -41,20 +41,23 @@ namespace ProgramOutputHook
             var process = new Process();
             var procStartInfo = new ProcessStartInfo(programToListenTo, programArgument)
             {
-                UseShellExecute = false,
+                UseShellExecute = false, // has to be false for redirecting output
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 StandardOutputEncoding = Encoding.UTF8
             };
+
             process.StartInfo = procStartInfo;
             process.EnableRaisingEvents = true;
             process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
             process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+
             process.Start();
+
             process.BeginOutputReadLine();
             process.BeginErrorReadLine();
 
-            Console.WriteLine($"Programme {programToListenTo} démarré avec l'argument '{programArgument}'");
+            Console.WriteLine($"Program {programToListenTo} started{(string.IsNullOrEmpty(programArgument) ? "." : $"with arguments : '{programArgument}'")}");
 
             process.WaitForExit();
         }
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿## ProgramOutputHook
+
+Demonstrates how one can easily hook to a program's output and display it in a console (or GUI app)
